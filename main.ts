8b2e7f829ccba5cc4f4e99876867d18b9c197097
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-10-14 14:40:41 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let degrees = 0
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (degrees < 135) {
+        basic.showArrow(ArrowNames.East)
+    } else if (degrees < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (degrees < 315) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
+})
